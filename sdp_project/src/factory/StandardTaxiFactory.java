b7dbe49fcd39e975e09d7efee98d6638a83f53b8
Adlet Baigaliev <153578967+aletgal@users.noreply.github.com>
2024-10-02 23:37:29 +0500

    Add files via upload

--- sdp_project/src/factory/StandardTaxiFactory.java
+++ sdp_project/src/factory/StandardTaxiFactory.java
@@ -1,4 +1,8 @@
 package factory;
 
-public class StandardTaxiFactory {
+public class StandardTaxiFactory extends Factory {
+    @Override
+    public Taxi createTaxi(String driverName, String license) {
+        return new StandardTaxi(driverName, license);
+    }
 }
